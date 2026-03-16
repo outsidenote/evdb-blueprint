1e74428b5ee03dbc75483ddd57208a3b2b6295dc
syedaskari <syed@liquidity-capital.com>
2026-03-16 22:41:36 +0400

    formatting the file

--- src/types/IdempotencyMessage.ts
+++ src/types/IdempotencyMessage.ts
@@ -3,22 +3,34 @@ import EvDbMessage from "@eventualize/types/messages/EvDbMessage";
 
 /**
  * Generates a unique idempotency key for the given business key and consumer ID.
- * @param idempotencyKey 
- * @param consumerId 
- * @returns 
+ * @param idempotencyKey
+ * @param consumerId
+ * @returns
  */
 export function getIdempotencyKey(idempotencyKey: string, consumerId: string): string {
-    return `${idempotencyKey}:${consumerId}`;
+  return `${idempotencyKey}:${consumerId}`;
 }
 
 /**
  * Creates an idempotency message from the given event and keys.
- * @param event 
- * @param idempotencyKey 
- * @param consumerId 
- * @returns 
+ * @param event
+ * @param idempotencyKey
+ * @param consumerId
+ * @returns
  */
-export function createIdempotencyMessageFromEvent(event: EvDbEvent, idempotencyKey: string, consumerId: string): EvDbMessage {
-    const key = getIdempotencyKey(idempotencyKey, consumerId);
-    return EvDbMessage.createFromEvent(event, { payloadType: `${consumerId}.IdempotencyKeyAddedForConsumer`, idempotencyKey: key, consumerId }, "idempotent");
+export function createIdempotencyMessageFromEvent(
+  event: EvDbEvent,
+  idempotencyKey: string,
+  consumerId: string,
+): EvDbMessage {
+  const key = getIdempotencyKey(idempotencyKey, consumerId);
+  return EvDbMessage.createFromEvent(
+    event,
+    {
+      payloadType: `${consumerId}.IdempotencyKeyAddedForConsumer`,
+      idempotencyKey: key,
+      consumerId,
+    },
+    "idempotent",
+  );
 }
