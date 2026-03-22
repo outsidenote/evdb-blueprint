60665c7d3c58108227c241eb3a86f69a162470d6
outsidenote <outsidenote@gmail.com>
2026-03-22 17:47:42 +0200

    Removed no-op view state handlers (#20)

--- src/BusinessCapabilities/Funds/swimlanes/Funds/views/SliceStateApproveWithdrawal/handlers.ts
+++ src/BusinessCapabilities/Funds/swimlanes/Funds/views/SliceStateApproveWithdrawal/handlers.ts
@@ -8,24 +8,8 @@ export const handlers = {
     event: FundsWithdrawalApproved,
   ): SliceStateApprovalWithdrawalViewState => ({ balance: state.balance - event.amount }),
 
-  FundsWithdrawalDeclined: (
-    state: SliceStateApprovalWithdrawalViewState,
-  ): SliceStateApprovalWithdrawalViewState => state,
-
   FundsDepositApproved: (
     state: SliceStateApprovalWithdrawalViewState,
     event: FundsDepositApproved,
   ): SliceStateApprovalWithdrawalViewState => ({ balance: state.balance + event.amount }),
-
-  WithdrawCommissionCalculated: (
-    state: SliceStateApprovalWithdrawalViewState,
-  ): SliceStateApprovalWithdrawalViewState => state,
-
-  FundsWithdrawn: (
-    state: SliceStateApprovalWithdrawalViewState,
-  ): SliceStateApprovalWithdrawalViewState => state,
-
-  FundsWithdrawDeclined: (
-    state: SliceStateApprovalWithdrawalViewState,
-  ): SliceStateApprovalWithdrawalViewState => state,
 };
